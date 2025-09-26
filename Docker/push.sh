ad77f336f3b7d602fc49c34b52f7c2ea837610d1
Laipi1240 <charles92421@gmail.com>
2025-09-26 12:35:55 +0800

    update Docker files

---
+++ Docker/push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+REPOSITORY="wilbur1240/isaacsim"
+TAG="5.0.0-ros2-humble"
+
+IMG="${REPOSITORY}:${TAG}"
+
+docker image push "${IMG}"
---
+++ Docker/run_headless.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+REPOSITORY="wilbur1240/isaacsim"
+TAG="5.0.0-ros2-humble"
+
+USER_NAME="arg"
+REPO_NAME="isaacsim"
+CONTAINER_NAME="isaac-5.0.0-ros2-humble-headless"
+
+CONTAINER_ID=$(docker ps -aqf "ancestor=${IMG}")
+if [ $CONTAINER_ID ]; then
+  echo "Attach to docker container $CONTAINER_ID"
+  xhost +
+  docker exec --privileged -e DISPLAY=${DISPLAY} -e LINES="$(tput lines)" -it ${CONTAINER_ID} bash
+  xhost -
+  return
+fi
+
+xhost +local:root  # allow X11
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+  xauth_list=$(xauth nlist $DISPLAY)
+  xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+  if [ ! -z "$xauth_list" ]; then
+    echo "$xauth_list" | xauth -f $XAUTH nmerge -
+  else
+    touch $XAUTH
+  fi
+  chmod a+r $XAUTH
+fi
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+  echo "[$XAUTH] was not properly created. Exiting..."
+  exit 1
+fi
+
+docker run \
+    -it \
+    --rm \
+    --user "root:root" \
+    --runtime=nvidia \
+    --gpus all \
+    --network=host \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    -e XAUTHORITY=$XAUTH \
+    -e REPO_NAME=$REPO_NAME \
+    -e HOME=/home/${USER_NAME} \
+    -e OPENAI_API_KEY=$OPENAI_API_KEY\
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e XDG_RUNTIME_DIR=/tmp/runtime-${USER_NAME} \
+    -e "ACCEPT_EULA=Y" \
+    -e "HEADLESS=1" \
+    -v /tmp/runtime-${USER_NAME}:/tmp/runtime-${USER_NAME} \
+    -v "$XAUTH:$XAUTH" \
+    -v "/home/${USER}/${REPO_NAME}:/home/${USER_NAME}/${REPO_NAME}" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -v "/usr/share/vulkan/icd.d/nvidia_icd.json:/usr/share/vulkan/icd.d/nvidia_icd.json" \
+    --name "${CONTAINER_NAME}" \
+    "${IMG}" \
+    bash
